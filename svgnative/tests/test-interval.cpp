44aeb0313c95812b437cd7a3433c244f486bee53
Moazin Khatti <moazinkhatri@gmail.com>
2021-05-01 22:39:45 +0500

    Changed `::min` to `::Min` and `::max` to `::Max` to avoid conflicts with similarly named macros in Windows.h that cause compilation problems. Other way is to undef those macros.

--- svgnative/tests/test-interval.cpp
+++ svgnative/tests/test-interval.cpp
@@ -20,10 +20,10 @@ TEST(interval_tests, ordering_test)
   SVGNative::Interval a(10, 12);
   SVGNative::Interval b(12, 10);
 
-  EXPECT_EQ(a.min(), 10);
-  EXPECT_EQ(a.max(), 12);
-  EXPECT_EQ(b.min(), 10);
-  EXPECT_EQ(b.max(), 12);
+  EXPECT_EQ(a.Min(), 10);
+  EXPECT_EQ(a.Max(), 12);
+  EXPECT_EQ(b.Min(), 10);
+  EXPECT_EQ(b.Max(), 12);
 }
 
 TEST(interval_tests, intersection_test)
@@ -39,24 +39,24 @@ TEST(interval_tests, intersection_test)
     SVGNative::Interval b(11, 14);
     SVGNative::Interval c = a & b;
     EXPECT_EQ((bool)c, true);
-    EXPECT_EQ(c.min(), 11);
-    EXPECT_EQ(c.max(), 14);
+    EXPECT_EQ(c.Min(), 11);
+    EXPECT_EQ(c.Max(), 14);
   }
   {
     SVGNative::Interval a(10, 15);
     SVGNative::Interval b(12, 18);
     SVGNative::Interval c = a & b;
     EXPECT_EQ((bool)c, true);
-    EXPECT_EQ(c.min(), 12);
-    EXPECT_EQ(c.max(), 15);
+    EXPECT_EQ(c.Min(), 12);
+    EXPECT_EQ(c.Max(), 15);
   }
   {
     SVGNative::Interval a(10, 15);
     SVGNative::Interval b(15, 20);
     SVGNative::Interval c = a & b;
     EXPECT_EQ((bool)c, true);
-    EXPECT_EQ(c.min(), 15);
-    EXPECT_EQ(c.max(), 15);
+    EXPECT_EQ(c.Min(), 15);
+    EXPECT_EQ(c.Max(), 15);
   }
   {
     SVGNative::Interval a(10, 15);
@@ -69,8 +69,8 @@ TEST(interval_tests, intersection_test)
     SVGNative::Interval b(10, 15);
     SVGNative::Interval c = a & b;
     EXPECT_EQ((bool)c, true);
-    EXPECT_EQ(c.min(), 14);
-    EXPECT_EQ(c.max(), 15);
+    EXPECT_EQ(c.Min(), 14);
+    EXPECT_EQ(c.Max(), 15);
   }
 
 }
